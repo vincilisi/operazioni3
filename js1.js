e2a23006e46bfb076cdce6600e1980b1712dc60b
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-21 18:59:47 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,13 @@
+let valori= prompt("scrivi i tuoi valori");
+let valori1= prompt("scrivi i tuoi valori");
+console.log(valori,valori1);
+
+let somma= valori+valori1;
+console.log(somma);
+let sottrazione= valori-valori1;
+let moltiplicazione= valori*valori1;
+let divisione=valori-valori1;
+let potenza= Math.pow(valori, 2);
+let potenza1= Math.pow(valori1, 2);
+
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono: somma" +somma+ " sottrazione"+sottrazione+" moltiplicazione"+moltiplicazione+" divisione"+divisione+" potenza"+potenza + "potenza 2"+potenza1);
